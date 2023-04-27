0da6c8d373dfee87570ece92a1df4021c6df3142
Boris Lazo <lazo_chita@mail.com>
2023-04-27 19:17:08 +0900

    Update brain-even logic

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from src.brain_even_logic import game_even
+from brain_games import welcome_user
+
+def main():
+    welcome_user()
+    game_even()
+
+if __name__ == "__main__":
+    main()
