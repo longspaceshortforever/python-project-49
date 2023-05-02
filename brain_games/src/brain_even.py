cea5480dc790a44bb769e9bd7e11555295b57416
Boris Lazo <lazo_chita@mail.com>
2023-05-02 19:04:46 +0900

    Update brain-even src

--- brain_games/src/brain_even.py
+++ brain_games/src/brain_even.py
@@ -1,5 +1,5 @@
-from .engine import welcome_user
-
+from .engine import welcome_user, generate_number
+import prompt
 
 COUNT_OF_CORRECT_ANSWERS = 3
 GAMES_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
@@ -8,3 +8,5 @@ GAMES_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 def game_even():
     welcome_user()
     print(GAMES_RULES)
+    CURRENT_NUMBER = generate_number
+    
