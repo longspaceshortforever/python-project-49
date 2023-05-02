5dfd7e3b7202d56264a8d2929bacd67d6a211343
Boris Lazo <lazo_chita@mail.com>
2023-05-02 18:51:26 +0900

    Add random function in engine

--- brain_games/src/engine.py
+++ brain_games/src/engine.py
@@ -1,7 +1,13 @@
 import prompt
-
+from random import randint 
 
 def welcome_user():
+    """Greeting user and gives name"""
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
+
+def generate_number():
+    """Generate random integral number"""
+    RANDOM_NUMBER = randint(0, 100)
+    print(RANDOM_NUMBER)
