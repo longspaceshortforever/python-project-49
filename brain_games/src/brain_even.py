11175e015774dcd2e8f47a616d07f812a69ca533
Boris Lazo <lazo_chita@mail.com>
2023-05-02 18:25:32 +0900

    rename brain_even src files

--- brain_games/src/brain_even.py
+++ brain_games/src/brain_even.py
@@ -1,8 +1,8 @@
 from src.engine import welcome_user
 import prompt
 
-count_of_correct_answers = 3
-game_rules = 'Answer "yes" if the number is even, otherwise answer "no".'
+COUNT_OF_CORRECT_ANSWERS = 3
+GAMES_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def game_even():
     welcome_user()
