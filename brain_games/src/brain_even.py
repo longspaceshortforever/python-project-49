a4d7311e6536c782c0c4cecd2ddc99914424a81d
Boris Lazo <lazo_chita@mail.com>
2023-05-05 19:30:28 +0900

    Update brain-even logic
    Added name of user as argument of iterate finction and count of answers function

--- brain_games/src/brain_even.py
+++ brain_games/src/brain_even.py
@@ -6,13 +6,13 @@ COUNT_OF_CORRECT_ANSWERS = 0
 GAMES_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def check_count_of_answers():
+def check_count_of_answers(user):
     global COUNT_OF_CORRECT_ANSWERS
     if COUNT_OF_CORRECT_ANSWERS == 3:
-        print("Congratulations, Bill!")
+        print("Congratulations, " + f'{user}' + "!")
 
 
-def iterate_game_even():
+def iterate_game_even(user):
     """Realize one cycle of game"""
     global COUNT_OF_CORRECT_ANSWERS
     if COUNT_OF_CORRECT_ANSWERS < 3:
@@ -28,17 +28,17 @@ def iterate_game_even():
         if correct_answer == answer:
             print("Correct!")
             COUNT_OF_CORRECT_ANSWERS = COUNT_OF_CORRECT_ANSWERS + 1
-            check_count_of_answers()
-            iterate_game_even()
+            check_count_of_answers(user)
+            iterate_game_even(user)
         else:
             wrong_answer_message = "\"" + f'{answer}' + "\" is wrong answer ;(."
             correct_answer_message = "Correct answer was \"" + f'{correct_answer}' + "\"."
-            advice = "\nLet's try again, Bill!"
-            wrong_message = what_is_wrong_answer + what_is_correct_answer + what_to_do
+            advice = "\nLet's try again, " + f'{user}' + "!"
+            wrong_message = wrong_answer_message + correct_answer_message + advice
             print(wrong_message)
 
 
 def game_even():
-    welcome_user()
+    name = welcome_user()
     print(GAMES_RULES)
-    iterate_game_even()
+    iterate_game_even(user=name)
--- brain_games/src/engine.py
+++ brain_games/src/engine.py
@@ -7,7 +7,7 @@ def welcome_user():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
-
+    return name
 
 def generate_number():
     """Generate random integral number"""
