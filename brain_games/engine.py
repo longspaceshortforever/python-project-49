0e94282afda69363a0ef27aa19a3b25602eb2ef9
Boris <lazo_chita@mail.ru>
2023-05-09 16:49:34 +0900

    Create brain_calc script function

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -32,14 +32,14 @@ def generate_wrong_message(user_answer, correct_answer, user_name):
 
 def run_game_engine(game):
     answers = 0
-    
+
     welcome_user()
     user_name = get_user_name()
     print("Hello, " + f'{user_name}')
     print(game.GAME_RULES)
-    
+
     for _ in ROUNDS:
-        
+
         question = game.make_question()
         print("Question: " + f'{question}')
         correct_answer = game.get_correct_answer(question)
@@ -47,11 +47,10 @@ def run_game_engine(game):
         if user_answer == correct_answer:
             print("Correct!")
             answers = answers + 1
-            
+
         else:
             generate_wrong_message(user_answer, correct_answer, user_name)
             break
-        
+
     if answers == 3:
         print("Congratulation, " + f'{user_name}' + "!")
-        
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+from ..games import brain_calc
+from ..engine import run_game_engine
 
 
 def main():
-    print("Welcome to brain_calc!")
+    run_game_engine(brain_calc)
 
 
 if __name__ == "__main__":
