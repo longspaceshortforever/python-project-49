1318a3a4b8e8da15eaf821c5bd663ed0783fa7ba
Boris <lazo_chita@mail.ru>
2023-05-09 21:55:31 +0900

    Realize brain-gcd logic in module and script, not checked

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,21 @@
+from random import randint
+
+
+GAME_RULES = 'Find the greatest common divisor of given numbers.'
+
+
+def generate_random_number():
+    return randint(0, 100)
+
+
+def make_task():
+    number1 = generate_random_number()
+    number2 = generate_random_number()
+    numbers = f'{number1}' + " " +  f'{number2}'
+    question = numbers
+    answer = get_correct_answer(number1, number2)
+    return (question, answer)
+
+
+def get_correct_answer(number1, number2):
+    return math.gcd(number1, number2)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from ..games import brain_gcd
+from ..engine import run_game_engine
+
+
+def main():
+    run_game_engine(brain_gcd)
+
+
+if __name__ == "__main__":
+    main()
