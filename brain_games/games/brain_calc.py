ac9d90042dd7039c68253c563fa73ea05f0a42a9
Boris <lazo_chita@mail.ru>
2023-05-09 16:50:27 +0900

    Add logic in brain_calc

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,41 @@
+from random import randint
+
+
+GAME_RULES = 'What is the result of the expression?'
+OPERATORS = ("+", "-", "*")
+
+
+def generate_random_number():
+    number = randint(0, 100)
+    return number
+
+
+def generate_random_operator():
+    index = randint(0, 2)
+    operator = OPERATORS[index]
+    return operator
+
+
+def calculate_expression(number1, number2, operator):
+    match operator:
+        case "+":
+            return number1 + number2
+        case "-":
+            return number1 - number2
+        case "*":
+            return number1 * number2
+
+
+def make_question():
+    number1 = generate_random_number()
+    number2 = generate_random_number()
+    operator = generate_random_operator()
+    answer = calculate_expression(number1, number2, operator)
+    question = f'{number1}' + f' {operator} ' + f'{number2}'
+    return (question, answer)
+
+
+def get_correct_answer(question):
+    if question % 2 == 0:
+        return "yes"
+    return "no"
