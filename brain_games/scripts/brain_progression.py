48fe48cf067116f79e8b5fb8c3732a835437122d
Boris Lazo <lazo_chita@mail.com>
2023-05-10 17:03:58 +0900

    Init brain-progression

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from ..games import brain_progression
+from ..engine import run_game_engine
+
+
+def main():
+    run_game_engine(brain_progression)
+
+
+if __name__ == "__main__":
+    main()
