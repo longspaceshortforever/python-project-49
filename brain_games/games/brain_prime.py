58c51b3522b8a032826f38200ba53ae55cbde354
Boris Lazo <lazo_chita@mail.com>
2023-05-17 15:34:14 +0900

    Fix is_prime func

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -12,6 +12,9 @@ def generate_random_number():
 
 def is_prime(number):
     k = 0
+    if number <= 1:
+        return False
+
     for i in range(2, number // 2 + 1):
         if (number % i == 0):
             k = k + 1
