526b3543477b8a9e8c7958775850c247b75e0590
Boris Lazo <lazo_chita@mail.com>
2023-06-01 17:02:59 +0900

    Fix get_answer func (add is_prime)

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -9,15 +9,20 @@ MAX_RANDOM_NUMBER = 100
 def generate_random_number():
     return randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
 
+def is_prime(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
 def make_task():
     number = generate_random_number()
     question = number
     answer = get_correct_answer(number)
-    return (question, answer)
+    return(question, answer)
 
 
 def get_correct_answer(number):
-    if number % 2 == 0:
+    if is_prime(number):
         return "yes"
     return "no"
